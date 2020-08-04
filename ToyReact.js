bc227c04b1890601becc64dde1bfca1e1c113018
廖宜鹏 <liaoyipeng@ut.cn>
2020-08-04 21:08:15 +0800

    feat: 实现 element text Wrapper;

--- ToyReact.js
+++ ToyReact.js
@@ -1,6 +1,49 @@
+
+class ElementWrapper {
+  constructor(type) {
+    this.root = document.createElement(type);
+  }
+  setAttribute(name, value) {
+    this.root = this.setAttribute(name, value);
+  }
+  appendChild(vchild) {
+    vchild.mountTo(this.root);
+  }
+  mountTo(parent) {
+    parent.appendChild(this.root);
+  }
+}
+
+export class Compoment {
+  mountTo(parent) {
+    let vdom = this.render();
+    vdom.mountTo(parent);
+  }
+  setAttribute(name, value) {
+    this[name] = value;
+  }
+}
+
+
+class TextWrapper {
+  constructor(content) {
+    this.root = document.createTextNode(content);
+  }
+  mountTo(parent) {
+    parent.appendChild(this.root);
+  }
+}
+
+
 export let ToyReact = {
   createElement(type, attributes, ...children) {
-    let element = document.createElement(type);
+    let element;
+    if (typeof type === 'string') {
+      element = new ElementWrapper(type);
+    } else {
+      element = new type;
+    }
+
     for (let name in attributes) {
       element.setAttribute(name, attributes[name]);
     }
@@ -8,10 +51,14 @@ export let ToyReact = {
     for (let child of children) {
       // 如果传入的是字符串，不是 node 节点，则创建 textNode 节点
       if (typeof child === 'string') {
-        child = document.createTextNode(child);
+        child = new TextWrapper(child);
       }
       element.appendChild(child);
     }
     return element;
+  },
+  render(vdom, element) {
+    // 让 vdom 去做 mountTo 这件事
+    vdom.mountTo(element);
   }
 };
--- main.js
+++ main.js
@@ -1,15 +1,27 @@
-import { ToyReact } from './toyReact';
+import { ToyReact, Compoment } from './toyReact';
 
-class Mycomponent {
+class Mycomponent extends Compoment {
+  render() {
+    return <div>
+      <span>hello</span>
+      <span>world</span>
 
+      <span>!!!</span>
+
+    </div>
+  }
 };
 
-// let a = <Mycomponent name="q"/>
+let a = <Mycomponent name="q"/>
 
-let a = <div name='a' id='id'>
-  <span>hello</span>
-  <span>world</span>
-  <span>!</span>
-</div>
+// let a = <div name='a' id='id'>
+//   <span>hello</span>
+//   <span>world</span>
+//   <span>!</span> 
+// </div>
 
-document.body.appendChild(a);
+// document.body.appendChild(a);
+ToyReact.render(
+  a,
+  document.body
+)
